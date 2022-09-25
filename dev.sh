a30383fb6160da4e1430dc1b8eb1e5889e69dc51
linozen <linus@sehn.tech>
2022-09-25 18:07:48 +0200

    New development script

---
+++ dev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+hugo server --disableFastRender --enableGitInfo --bind="0.0.0.0" --port 3000
--- view-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-hugo server --disableFastRender --i18n-warnings --enableGitInfo --bind="0.0.0.0" --port 1316 
